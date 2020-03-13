931de81bef9d0b4e22515a1b702cede510860b90
Zedd <zedd.0202@kakaocorp.com>
2020-03-13 23:59:19 +0900

    first commit

---
+++ SonarQubeFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SonarQubeFinal
+//
+//  Created by Zedd on 2020/03/13.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import UIKit
+class Zedd {
+    
+    func testZedd() -> Bool {
+        return true
+    }
+}
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SonarQubeFinalAppNameTests/SonarQubeFinalAppNameTests.swift
@@ -0,0 +1,33 @@
+//
+//  SonarQubeFinalAppNameTests.swift
+//  SonarQubeFinalAppNameTests
+//
+//  Created by Zedd on 2020/03/13.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import XCTest
+
+class SonarQubeFinalAppNameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
